7f1effcbf1a96e648a85cc3cdcbc6aacf2445446
Shahabr99 <sean.rohani9@gmail.com>
2023-03-18 17:46:26 -0700

    another one bites the dust

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,17 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    vowels = 'aeiou'
+    result = {}
+    
+    new_phrase = phrase.lower()
+    for letter in new_phrase:
+        if vowels.find(letter) != -1:
+            
+            if letter in result: 
+                v = result.get(letter) + 1
+                result[letter] = v
+            else:
+                result[letter] = 1
+    print(result)
+        
